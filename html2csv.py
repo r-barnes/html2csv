15d3606850d3323f55ef735f84a970fd42420554
Richard Barnes <rbarnes@umn.edu>
2020-06-20 11:58:22 -0600

    Fix argument handling and make some simplifications

--- html2csv.py
+++ html2csv.py
@@ -1,46 +1,35 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #Richard's html2csv converter
-#rbarnes@umn.edu
+#rijard.barnes@gmail.com
 #
 
 from bs4 import BeautifulSoup
-import sys
-import csv
 import argparse
+import csv
+import fileinput
+import sys
 
 parser = argparse.ArgumentParser(description='Reads in an HTML and attempts to convert all tables into CSV files.')
 parser.add_argument('--delimiter', '-d', action='store', default=',',help="Character with which to separate CSV columns")
 parser.add_argument('--quotechar', '-q', action='store', default='"',help="Character within which to nest CSV text")
-parser.add_argument('filename',nargs="?",help="HTML file from which to extract tables")
+parser.add_argument('filename', nargs="?", help="HTML file from which to extract tables")
 args = parser.parse_args()
 
-if sys.stdin.isatty() and not args.filename:
-  parser.print_help()
-  sys.exit(-1)
-elif not sys.stdin.isatty():
-  args.filename = sys.stdin
-else:
-  args.filename = open(sys.argv[1],'r')
+data = list(fileinput.input(files=(args.filename, ) if args.filename is not None else ('-', )))
+data = "\n".join(data)
 
-print("Opening file")
-fin  = args.filename.read()
+soup = BeautifulSoup(data, "html.parser")
 
-print("Parsing file")
-soup = BeautifulSoup(fin,"html.parser")
-
-print("Preemptively removing unnecessary tags")
+#Preemptively removing unnecessary tags
 [s.extract() for s in soup('script')]
 
-print("CSVing file")
-tablecount = -1
-for table in soup.findAll("table"):
-  tablecount += 1
-  print("Processing Table #%d" % (tablecount))
-  with open(sys.argv[1]+str(tablecount)+'.csv', 'w', newline='') as csvfile:
-    fout = csv.writer(csvfile, delimiter=args.delimiter, quotechar=args.quotechar, quoting=csv.QUOTE_MINIMAL)
-    for row in table.findAll('tr'):
-      cols = row.findAll(['td','th'])
-      if cols:
-        cols = [str(x.text).strip() for x in cols]
-        fout.writerow(cols)
+for index,table in enumerate(soup.findAll("table")):
+  fout = csv.writer(sys.stdout, delimiter=args.delimiter, quotechar=args.quotechar, quoting=csv.QUOTE_MINIMAL)
+  for row in table.findAll('tr'):
+    cols = row.findAll(['td','th'])
+    if cols:
+      cols = [str(x.text).strip() for x in cols]
+      fout.writerow(cols)
+
+  print("\n\n###")
